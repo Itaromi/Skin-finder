f14d631f2c16bcdcd13520ebcfd2dae612e8f2b5
Itaromi <51408432+Itaromi@users.noreply.github.com>
2023-05-12 00:35:27 +0200

    Update skin.js
    var -> const

--- skin.js
+++ skin.js
@@ -1,9 +1,9 @@
 function loadSkin() {
-  var username = document.getElementById("username").value;
-  var skinURL = "https://minotar.net/avatar/" + username + "/100.png";
-  var skinCommand = "/give @s minecraft:player_head{SkullOwner:"+ username +"}";
-  var skinPreview = document.getElementById("skin-preview");
-  var skinCommandPreview = document.getElementById("skin-command");
+  const username = document.getElementById("username").value;
+  const skinURL = "https://minotar.net/avatar/" + username + "/100.png";
+  const skinCommand = "/give @s minecraft:player_head{SkullOwner:"+ username +"}";
+  const skinPreview = document.getElementById("skin-preview");
+  const skinCommandPreview = document.getElementById("skin-command");
   skinPreview.innerHTML = `<img src= ${skinURL} >`;
   skinCommandPreview.innerHTML = `<p> ${skinCommand} <p>`;
 } 
